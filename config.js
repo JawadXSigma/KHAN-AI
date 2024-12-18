587b1da6f5e44db4976912bf3fd815b1de4e0ae5
JawadXSigma <jawadkoyeb@outlook.com>
2024-12-18 12:02:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "true",
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*Status Seen By KHAN-AI 🖤*",
 // set the auto reply massage on status reply    
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
